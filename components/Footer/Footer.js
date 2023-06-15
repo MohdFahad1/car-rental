8f63465ae96ab3ce9ceb3cdb40436b7c7ec52d0b
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-06-15 13:07:37 +0530

    Responsiveness added in Footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,35 +6,39 @@ const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
-        <div className={styles.footer_first}>
-          <h1>CAR <span>Rental</span></h1>
-          <p>We offers a big range of vehicles for all your driving needs. We have the perfect car to meet your needs</p>
-          <div className={styles.footer_first_contact}>
-            <p><FaPhone style={{marginRight:"10px"}}/>(123)-456-789</p>
-            <p><FaEnvelope style={{marginRight:"10px"}}/>carrental@gmail.com</p>
-            <p>Design by XpeedStudio</p>
+        <div className={styles.main_one}>
+          <div className={styles.footer_first}>
+            <h1>CAR <span>Rental</span></h1>
+            <p>We offers a big range of vehicles for all your driving needs. We have the perfect car to meet your needs</p>
+            <div className={styles.footer_first_contact}>
+              <p><FaPhone style={{ marginRight: "10px" }} />(123)-456-789</p>
+              <p><FaEnvelope style={{ marginRight: "10px" }} />carrental@gmail.com</p>
+              <p>Design by XpeedStudio</p>
+            </div>
+          </div>
+          <div className={styles.footer_second}>
+            <h1>COMPANY</h1>
+            <p>New York</p>
+            <p>Careers</p>
+            <p>Mobile</p>
+            <p>Blog</p>
+            <p>How we work</p>
           </div>
         </div>
-        <div className={styles.footer_second}>
-          <h1>COMPANY</h1>
-          <p>New York</p>
-          <p>Careers</p>
-          <p>Mobile</p>
-          <p>Blog</p>
-          <p>How we work</p>
-        </div>
-        <div className={styles.footer_third}>
-          <h1>WORKING HOURS</h1>
-          <p>Mon - Fri: 9:00AM - 9:00PM</p>
-          <p>Sat: 9:00AM - 7:00PM</p>
-          <p>Sun: Closed</p>
-        </div>
-        <div className={styles.footer_fourth}>
-          <h1>SUBSCRIPTION</h1>
-          <p>Subscribe your Email address for latest news & updates</p>
-          <div></div>
-            <input type="email" placeholder='Enter Email Address'/>
+        <div className={styles.main_two}>
+          <div className={styles.footer_third}>
+            <h1>WORKING HOURS</h1>
+            <p>Mon - Fri: 9:00AM - 9:00PM</p>
+            <p>Sat: 9:00AM - 7:00PM</p>
+            <p>Sun: Closed</p>
+          </div>
+          <div className={styles.footer_fourth}>
+            <h1>SUBSCRIPTION</h1>
+            <p>Subscribe your Email address for latest news & updates</p>
+            <div></div>
+            <input type="email" placeholder='Enter Email Address' />
             <button>Submit</button>
+          </div>
         </div>
       </footer>
     </>
