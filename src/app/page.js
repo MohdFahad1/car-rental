410711a6ce3e9bdf343857892f9dd83cc33df7a7
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-06-17 11:04:54 +0530

    Home page banner completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+import styles from './page.module.css';
 import Hero from "../../components/Home/Hero/Hero";
 import Hero2 from "../../components/Home/Hero2/Hero2";
 import Hero3 from "../../components/Home/Hero3/Hero3";
+import Hero4 from "../../components/Home/Hero4/Hero4";
 
 export default function Home() {
   return (
@@ -8,6 +10,11 @@ export default function Home() {
       <Hero />
       <Hero2 />
       <Hero3 />
+      <Hero4 />
+      <div className={styles.banner}>
+        <h1>Save big with our cheap car rental!</h1>
+        <h3>Top Airports. Local Suppliers. <span>24/7</span> Support</h3>
+      </div>
     </>
   )
 }
