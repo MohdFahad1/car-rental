1bd915e26cbb07a5487c11d00e9f31e4efe90e59
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-07-25 11:43:08 +0530

    Vehicle Model Page Completed

---
+++ src/app/vehicle/data.js
@@ -0,0 +1,47 @@
+import car1 from '../../../public/car1.png';
+import car2 from '../../../public/car2.png';
+import car3 from '../../../public/car3.png';
+import car4 from '../../../public/car4.png';
+import car5 from '../../../public/car5.png';
+import car6 from '../../../public/car6.png';
+
+export const data = [
+    {
+        id:1,
+        name:"Audi A1",
+        price:"$45",
+        company:"Audi",
+        image:car1,
+    },
+    {
+        id:2,
+        name:"Golf 6",
+        price:"$37",
+        company:"VW",
+        image:car2,
+    },{
+        id:3,
+        name:"Toyota",
+        price:"$30",
+        company:"Camry",
+        image:car3,
+    },{
+        id:4,
+        name:"BMW 320",
+        price:"$35",
+        company:"ModernLine",
+        image:car4,
+    },{
+        id:5,
+        name:" Mercedes",
+        price:"$50",
+        company:"Benz GLK",
+        image:car5,
+    },{
+        id:6,
+        name:"VW Passat",
+        price:"$25",
+        company:"CC",
+        image:car6,
+    },
+]
--- src/app/vehicle/page.js
+++ src/app/vehicle/page.js
@@ -1,14 +1,72 @@
 import React from 'react'
 import Common from '../../../components/CommonBg/Common';
+import {data as vehicleData} from './data';
+import Image from 'next/image';
+import styles from './page.module.css';
+import Link from 'next/link';
+import { FaCarSide, FaStar } from 'react-icons/fa';
 
 const page = () => {
   const data = {
     name:"Vehicle Models",
   };
   return (
+    <>
     <div>
       <Common myData={data}/>
     </div>
+    <div className={styles.main}>
+    <div className={styles.models}>
+      {vehicleData.map((vehicle) => {
+        const { id, name, price, company, image } = vehicle;
+        return (
+          <div key={id} className={styles.model_card}>
+
+          {/* model image  */}
+            <div className={styles.model_img}>
+                <Image src={image} alt={name} height={250} width={350}/>
+            </div>
+
+            <div style={{paddingLeft:"25px", paddingRight:"25px", marginTop:"20px"}}>
+            {/* Model name */}
+            <div style={{display:"flex", justifyContent:"space-between"}}>
+              <div>
+                <h1>{name}</h1>
+                <h3><FaStar /></h3>
+              </div>
+              <div>
+                <h1>{price}</h1>
+                <p>per day</p>
+              </div>
+            </div>
+
+            <div style={{marginTop:"20px"}}>
+              <div style={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
+                <p style={{fontSize:"20px", display:"flex", gap:"10px", alignItems:"center"}}><FaCarSide /> <span style={{color:"gray", fontWeight:"500"}}>{company}</span></p>
+                <p style={{fontSize:"20px", display:"flex", gap:"10px", alignItems:"center"}}><span style={{color:"gray", fontWeight:"500"}}>4/5</span> <FaCarSide /></p>
+              </div>
+              <div style={{display:"flex", justifyContent:"space-between"}}>
+                <p style={{fontSize:"20px", display:"flex", gap:"10px", alignItems:"center"}}><FaCarSide /> <span style={{color:"gray", fontWeight:"500"}}>Manual</span></p>
+                <p style={{fontSize:"20px", display:"flex", gap:"10px", alignItems:"center"}}><span style={{color:"gray", fontWeight:"500"}}>Diesel</span> <FaCarSide /></p>
+              </div>
+            </div>
+
+            </div>
+
+            <hr style={{margin:"25px"}}/>
+
+            <div style={{display:"flex", justifyContent:"center"}}>
+            <Link href="/">
+              <button className={styles.book}>Book Ride</button>
+            </Link>
+            </div>
+
+          </div>
+        )
+      })}
+    </div>
+    </div>
+    </>
   )
 }
 
