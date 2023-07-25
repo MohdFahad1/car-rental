9d7ee14e15441c347d3b9a39d22aede0cc8ff5b6
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-07-25 12:16:37 +0530

    Members Page Completed

---
+++ src/app/team/data.js
@@ -0,0 +1,45 @@
+import avatar1 from '../../../public/avatar1.png';
+import avatar2 from '../../../public/avatar2.png';
+import avatar3 from '../../../public/avatar3.png';
+import avatar4 from '../../../public/avatar4.png';
+import avatar5 from '../../../public/avatar5.png';
+import avatar6 from '../../../public/avatar6.png';
+
+export const data = [
+    {
+        id:1,
+        name:"Luke Miller",
+        designation:"Salesman",
+        image: avatar1,
+    },
+    {
+        id:2,
+        name:"Michael Diaz",
+        designation:"Business Owner",
+        image: avatar2,
+    },
+    {
+        id:3,
+        name:"Briana Ross",
+        designation:"Photographer",
+        image: avatar3,
+    },
+    {
+        id:4,
+        name:"Lauren Rivera",
+        designation:"Car Detailist",
+        image: avatar4,
+    },
+    {
+        id:5,
+        name:"Martin Rizz",
+        designation:"Mechanic",
+        image: avatar5,
+    },
+    {
+        id:6,
+        name:"Caitlyn Hunt",
+        designation:"Manager",
+        image: avatar6,
+    },
+]
--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import Common from '../../../components/CommonBg/Common';
+import Image from 'next/image';
+import {data as Members} from './data';
+import styles from './page.module.css';
 
 const page = () => {
   const data = {
     name:"Our Team",
   };
   return (
-    <div>
-      <Common myData={data}/>
-    </div>
+    <>
+      <div>
+        <Common myData={data}/>
+      </div>
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <div className={styles.main}>
+          {Members.map((members) => {
+            const {id, name, designation, image} = members;
+            return (
+              <div key={id} className={styles.card}>
+              <Image src={image} alt={name} height={300} width={300}/>
+              <h2 style={{marginTop:"10px"}}>{name}</h2>
+              <p style={{color:"gray", fontWeight:"500"}}>{designation}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
