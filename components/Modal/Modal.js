42fd06991fd39a457f4e8981162a04ee2c9b338e
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-07-30 17:34:00 +0530

    Modal Styling Completed

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -1,150 +1,178 @@
 "use client"
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './Modal.module.css';
-import { FaCross, FaInfoCircle, FaCalendarAlt } from 'react-icons/fa';
+import { FaInfoCircle, FaCalendarAlt } from 'react-icons/fa';
+import { ImCross } from "react-icons/im";
 import { MdLocationOn } from "react-icons/md";
 import Image from 'next/image';
- 
 
-const ModalComponent = ({car, pick, drop, pickDate, dropDate, carImg, isOpen, setIsOpen}) => {
+
+const ModalComponent = ({ car, pick, drop, pickDate, dropDate, carImg, isOpen, setIsOpen }) => {
 
     // const showData = () => {
     //     console.log(car+" "+pick+" "+drop+" "+dropDate+" "+pickDate);
     // }
-  
+
+    console.log(carImg);
+
     return (
         <>
+            <div className={styles.modal}>
             <div>
 
-                <div>
+                {/* COMPLETE REGISATRATION HEADING */}
+                <div className={styles.top_heading}>
                     <span>COMPLETE REGISTRATION</span>
-                    <span><FaCross /></span>
+                    <span style={{cursor:"pointer"}}><ImCross /></span>
                 </div>
 
-                <div>
-                    <div><FaInfoCircle />Upon completing this reservation enquiry, you will receive:</div>
-                    <div>Your rental voucher to produce on arrival at the rental desk and a toll-free customer support number.</div>
+                {/* UPON COMPLETION DESCRIPTION */}
+                <div className={styles.upon_completion}>
+                    <div className={styles.div1}><FaInfoCircle style={{marginRight:"5px"}}/>Upon completing this reservation enquiry, you will receive:</div>
+                    <div className={styles.div2}>Your rental voucher to produce on arrival at the rental desk and a toll-free customer support number.</div>
                 </div>
 
-                <div>
+
+                <div className={styles.booking_desc}>
                     <div>
-                        <h1>Location & Date</h1>
+                        <h2 style={{color:"#FF4D30"}}>Location & Date</h2>
 
 
-                        <div>
+                        <div style={{marginTop:"30px"}}>
                             <div>
-                                <span><FaCalendarAlt /></span>
-                                <span>Pick-Up Date & Time</span>
+                                <span style={{color:"gray", fontSize:"18px"}}><FaCalendarAlt /></span>
+                                <span style={{fontWeight:"bold", fontSize:"18px", marginLeft:"10px"}}>Pick-Up Date & Time</span>
                             </div>
                             <div>
-                                <span>{pickDate}/</span>
+                                <span style={{color:"gray", marginLeft:"30px"}}>{pickDate} / </span>
                                 <input type="time" />
                             </div>
                         </div>
-
-
-                        <div>
+                        <div style={{marginTop:"30px"}}>
                             <div>
-                                <span><FaCalendarAlt /></span>
-                                <span>Pick-Up Date & Time</span>
+                                <span style={{color:"gray", fontSize:"18px"}}><FaCalendarAlt /></span>
+                                <span style={{fontWeight:"bold", fontSize:"18px", marginLeft:"10px"}}>Pick-Up Date & Time</span>
                             </div>
                             <div>
-                                <span>{pickDate}/</span>
+                                <span style={{color:"gray", marginLeft:"30px"}}>{pickDate} / </span>
                                 <input type="time" />
                             </div>
                         </div>
-
-
-                        <div>
+                        <div style={{marginTop:"30px"}}>
                             <div>
-                                <span><MdLocationOn /></span>
-                                <span>Pick-Up Location</span> 
+                                <span style={{color:"gray", fontSize:"18px"}}><MdLocationOn /></span>
+                                <span style={{fontWeight:"bold", fontSize:"18px", marginLeft:"10px"}}>Pick-Up Location</span>
                             </div>
-                            <div>{pick}</div>
+                            <div style={{color:"gray", marginLeft:"30px"}}>{pick}</div>
                         </div>
-
-
-                        <div>
+                        <div style={{marginTop:"30px"}}>
                             <div>
-                                <span><MdLocationOn /></span>
-                                <span>Drop-off Location</span> 
+                                <span style={{color:"gray", fontSize:"18px"}}><MdLocationOn /></span>
+                                <span style={{fontWeight:"bold", fontSize:"18px", marginLeft:"10px"}}>Drop-off Location</span>
                             </div>
-                            <div>{drop}</div>
+                            <div style={{color:"gray", marginLeft:"30px"}}>{drop}</div>
                         </div>
-
                     </div>
-                    <div>
-                        <h3>Car - <span>{car}</span></h3>
-                        <Image src={carImg} alt={car} height={200} width={200}/>
+
+                    <div className={styles.car_image}>
+                        <h2>Car - <span style={{color:"#FF4D30"}}>{car}</span></h2>
+                        {/* <Image src={carImg} alt={car} height={200} width={200} /> */}
                     </div>
 
 
                 </div>
 
-                <hr />
 
-                <div>
-                <div>PERSONAL INFORMATION</div>
 
-                    <form>
+                <hr /> {/* Separator */}
 
-                        <div>
-                            <label>First Name <span>*</span></label>
-                            <input type="text" placeholder="Enter your first name"/>
-                        </div>
-                        
-                        <div>
-                            <label>Last Name <span>*</span></label>
-                            <input type="text" placeholder="Enter your Last name"/>
-                        </div>
 
-                        
-                        <div>
-                            <label>Phone Number <span>*</span></label>
-                            <input type="number" placeholder="Enter your phone number" className={styles.numberInput}/>
-                        </div>
+                {/* Personal Informatoin Section */}
 
-                        
-                        <div>
-                            <label>Age <span>*</span></label>
-                            <input type="number" placeholder="18"/>
+                <div className={styles.personal}>
+
+                    <h2>PERSONAL INFORMATION</h2>
+
+                    <form className={styles.personal_form}>
+
+                        <div className={styles.outer_div}>
+                            <div className={styles.inner_div}>
+                                <label>First Name <span>*</span></label>
+                                <input type="text" placeholder="Enter your first name" />
+                                <p>This field is required.</p>
+                            </div>
+
+                            <div className={styles.inner_div}>
+                                <label>Last Name <span>*</span></label>
+                                <input type="text" placeholder="Enter your Last name" />
+                                <p>This field is required.</p>
+                            </div>
                         </div>
 
-                        
-                        <div>
-                            <label>Email <span>*</span></label>
-                            <input type="email" placeholder="Enter your email address"/>
+                        <div className={styles.outer_div}>
+                            <div className={styles.inner_div}>
+                                <label>Phone Number <span>*</span></label>
+                                <input type="number" placeholder="Enter your phone number" className={styles.numberInput} />
+                                <p>This field is required.</p>
+                            </div>
+
+
+                            <div className={styles.inner_div}>
+                                <label>Age <span>*</span></label>
+                                <input type="number" placeholder="18" />
+                                <p>This field is required.</p>
+                            </div>
                         </div>
 
-                        
-                        <div>
-                            <label>Address <span>*</span></label>
-                            <input type="text" placeholder="Enter your street address"/>
+
+
+                        {/* EMAIL */}
+                        <div className={styles.email_address}>
+                            <div className={styles.inner_div}>
+                                <label>Email <span>*</span></label>
+                                <input type="email" placeholder="Enter your email address" className={styles.my_email} />
+                                <p>This field is required.</p>
+                            </div>
+
+                            {/* ADDRESS */}
+                            <div className={styles.inner_div}>
+                                <label>Address <span>*</span></label>
+                                <input type="text" placeholder="Enter your street address" className={styles.address} />
+                                <p>This field is required.</p>
+                            </div>
                         </div>
 
-                        
-                        <div>
-                            <label>City <span>*</span></label>
-                            <input type="text" placeholder="Enter your city"/>
+                        {/* CITY & ZIPCODE */}
+                        <div className={styles.outer_div}>
+                            <div className={styles.inner_div}>
+                                <label>City <span>*</span></label>
+                                <input type="text" placeholder="Enter your city" />
+                                <p>This field is required.</p>
+                            </div>
+
+
+                            <div className={styles.inner_div}>
+                                <label>Zip Code <span>*</span></label>
+                                <input type="number" placeholder="Enter your zip code" className={styles.numberInput} />
+                                <p>This field is required.</p>
+                            </div>
                         </div>
 
-                        
-                        <div>
-                            <label>Zip Code <span>*</span></label>
-                            <input type="number" placeholder="Enter your zip code" className={styles.numberInput}/>
-                        </div>
-
-                        <div>
-                            <input type='checkbox' />
+                        <div className={styles.checkbox_div}>
+                            <input type='checkbox' className={styles.checkbox} />
                             <p>Please send me latest news and updates</p>
                         </div>
 
-                        <div>
-                            <button>Reserve Now</button>
+
+                        {/* Buttton Reserve Now */}
+                        <div className={styles.btn}>
+                            <div>
+                                <button>Reserve Now</button>
+                            </div>
                         </div>
                     </form>
                 </div>
-
+                </div>
             </div>
         </>
     )
